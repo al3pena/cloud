68f5f1b502b5d2cdc51d31220acbb78c2fe7daa6
al3pena <131722878+al3pena@users.noreply.github.com>
2024-05-16 21:00:20 -0300

    Create random.php
    aletorio inicial

---
+++ random.php
@@ -0,0 +1,19 @@
+<?php
+
+function randomAleatorio($longitud = 12) {
+
+    $caracteresValidos = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIUEWNDJUAS";
+    $codigoRnd = "";
+
+    for($i = 0; $i< $longitud; $++){
+
+      $codigoRnd .= $caracteresValidos[rand(0, strlen($caracteresValidos) -1)];
+    }  
+
+  return $codigoRnd;
+  
+}
+
+$codigoAleatorio = randomAleatorio();
+
+  echo "El codigo random es de: " . $codigoAleatorio;
