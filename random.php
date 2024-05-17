515590a8eaf9232468e2192bb794c61d83004096
al3pena <131722878+al3pena@users.noreply.github.com>
2024-05-16 21:07:01 -0300

    Update random.php

--- random.php
+++ random.php
@@ -5,15 +5,14 @@ function randomAleatorio($longitud = 12) {
     $caracteresValidos = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIUEWNDJUAS";
     $codigoRnd = "";
 
-    for($i = 0; $i< $longitud; $++){
+    for($i = 0; $i< $longitud; $i++){
 
       $codigoRnd .= $caracteresValidos[rand(0, strlen($caracteresValidos) -1)];
     }  
 
   return $codigoRnd;
-  
 }
 
 $codigoAleatorio = randomAleatorio();
-
-  echo "El codigo random es de: " . $codigoAleatorio;
+echo "El codigo random es de: " . $codigoAleatorio;
+?>
