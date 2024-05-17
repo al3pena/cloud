bdf3385df2b1099281b3a257a52f219de14fcfe7
al3pena <131722878+al3pena@users.noreply.github.com>
2024-05-16 21:04:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
-<?
-echo "Hola mundo";
+<?php
+
+function randomAleatorio($longitud = 12) {
+
+    $caracteresValidos = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIUEWNDJUAS";
+    $codigoRnd = "";
+
+    for($i = 0; $i< $longitud; $++){
+
+      $codigoRnd .= $caracteresValidos[rand(0, strlen($caracteresValidos) -1)];
+    }  
+
+  return $codigoRnd;
+  
+}
+
+$codigoAleatorio = randomAleatorio();
+
+  echo "El codigo random es de: " . $codigoAleatorio;
